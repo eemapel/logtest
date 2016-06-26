3b1bf4ef066828ab51260b64812336ab0505d129
Marko Pellikka <eemapel@gmail.com>
2016-06-26 10:03:48 -0700

    Moved log processing function to a lib

--- logtest.js
+++ logtest.js
@@ -4,6 +4,8 @@ var fs = require("fs")
 var readline = require("readline")
 var wlog = require('wlog')
 
+var logproc = require('./lib/logproc.js')
+
 wlog.tag("TESTLOG")
 
 // Normalize argument array
@@ -68,62 +70,11 @@ fs.exists(logfile, (exists) => {
               lines.push(line)
            }).on('close', function () {
               wlog.note("Logs read successfully..")
-              log_processor(lines)
+              logproc.log_processor(lines, ylist, nlist)
            })
        })
     })
 })
-
-function log_processor(lines) {
-    console.log("")
-    wlog.note("Starting the testing..")
-    console.log("")
-
-    // Counter for issues
-    var failures = 0;
-
-    lines.forEach(function(line, number) {
-        ylist.forEach(function(re, index, object) {
-            if(re.test(line)) {
-                wlog.note("Found required match with condition " + re + " on line number " + number)
-                console.log(line)
-                console.log("")
-
-                // Remove element from array
-                object.splice(index, 1)
-            }
-        })
-
-        nlist.forEach(function(re) {
-            if(re.test(line)) {
-                wlog.remark("Found disallowed match with condition " + re + " on line number " + number)
-                console.log(line)
-                console.log("")
-
-                failures++
-            }
-        })
-    })
-
-    console.log("")
-    wlog.note("Summary for processed log")
-    wlog.note("=========================")
-    wlog.note("Not allowed matches : " + failures)
-    wlog.note("Matches not found   : " + ylist.length)
-    console.log(ylist)
-    console.log("")
-
-    // The verdict
-    if (failures || ylist.length) {
-        wlog.remark("==> Test failed!")
-        console.log("")
-        process.exit(1)
-    } else {
-        wlog.note("==> Test succeeced!")
-        console.log("")
-        process.exit(0)
-    }
-}
 
 function help() {
     wlog.note("Usage:")

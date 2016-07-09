54c2c152b85bd3041507e62a3a442a6afc3bf863
Marko Pellikka <eemapel@gmail.com>
2016-07-09 16:36:09 -0700

    Now expecting command to be executed instead of log file
    This is breaking backward compatibility, but gives more options how to use this
    tool. Also as the command may have its own arguments, it has to be the last,
    meaning the rulesfile will be expected before.

---
+++ HISTORY.md
@@ -0,0 +1,2 @@
+v2.0.0
+- Breaking backward compatibility, now expecting command execution instead of reading existing log file
--- logtest.js
+++ logtest.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 
-var fs = require("fs")
-var readline = require("readline")
+var fs = require('fs')
+var readline = require('readline')
+var cp = require('child_process')
 var wlog = require('wlog')
 
 var logproc = require('./lib/logproc.js')
@@ -11,9 +12,12 @@ wlog.tag("TESTLOG")
 // Normalize argument array
 var args = process.argv.slice(2)
 
-// Assume the first argument as the file
-logfile = args[0]
-rulesfile = args[1]
+// Assume the first argument as rules file
+var rulesfile = args[0]
+
+// Assume the second argument as command or script to execute
+var executable = args[1]
+var exec_args = args.slice(2)
 
 // List for failure (no) conditions
 var nlist = []
@@ -24,61 +28,64 @@ var ylist = []
 // placeholder for lines
 var lines = []
 
-if (logfile === undefined || rulesfile === undefined)
+if (executable === undefined || rulesfile === undefined)
     help()
 
-fs.exists(logfile, (exists) => {
+fs.exists(rulesfile, (exists) => {
     if (!exists)
-        wlog.err("log-file not found: " + logfile)
+        wlog.err("rules-file not found: :" + rulesfile)
 
-   fs.exists(rulesfile, (exists) => {
-        if (!exists)
-            wlog.err("rules-file not found: " + rulesfile)
+    var rf = readline.createInterface({
+        input: fs.createReadStream(rulesfile)
+    })
 
-        var rf = readline.createInterface({
-            input: fs.createReadStream(rulesfile)
-        })
+    rf.on('line', function(line) {
+        if (line.length) {
+            var cond = line[0]
+            var regex_string = line.substring(line.indexOf("/") + 1, line.lastIndexOf("/"))
+            if (!regex_string || regex_string == "/")
+                wlog.err("invalid regex string")
 
-        var lf = readline.createInterface({
-            input: fs.createReadStream(logfile)
-        })
-
-        rf.on('line', function (line) {
-            if (line.length) {
-                var cond = line[0]
-                var regex_string = line.substring(line.indexOf("/") + 1, line.lastIndexOf("/"))
-                if (!regex_string || regex_string == "/")
-                    wlog.err("invalid regex string")
-
-                if (cond == "y") {
-                    ylist.push(new RegExp(regex_string))
-                } else if (cond == "n") {
-                    nlist.push(new RegExp(regex_string))
-                } else {
-                    wlog.err("invalid condition type")
-                }
+            if (cond == "y") {
+                ylist.push(new RegExp(regex_string))
+            } else if (cond == "n") {
+                nlist.push(new RegExp(regex_string))
+            } else {
+                wlog.err("invalid condition type")
             }
-       }).on('close', function () {
-           wlog.note("Rules read successfully..")
-
-           // Now we can read the log-file
-           var lf = readline.createInterface({
-               input: fs.createReadStream(logfile)
-           })
-
-           lf.on('line', function (line) {
-              lines.push(line)
-           }).on('close', function () {
-              wlog.note("Logs read successfully..")
-              logproc.log_processor(lines, ylist, nlist)
-           })
-       })
+        }
+    }).on('close', function() {
+        wlog.note("Rules read successfully..")
+        command_runner()
     })
 })
 
+function command_runner() {
+    fs.exists(executable, (exists) => {
+        var child
+        if (exists) {
+            // If executable exists as local file, it's a script
+            child = cp.execFile(__dirname + "/" + executable[0], { timeout: 5000 })
+        } else {
+            // Try run it as a command
+            child = cp.spawn(executable, exec_args, { timeout: 5000 })
+        }
+
+        child.stdout.on('data', function(data) {
+            // Concatenate lines to array
+            lines = lines.concat(data.toString().split(/\r?\n/))
+        })
+
+        child.stdout.on('end', function() {
+            wlog.note("Logs read successfully..")
+            logproc.log_processor(lines, ylist, nlist)
+        })
+    })
+}
+
 function help() {
     wlog.note("Usage:")
-    wlog.note("$ logtest <log-file> <rules-file>")
+    wlog.note("$ logtext <rules-file> <command/script>")
     process.exit(0)
 }
 
